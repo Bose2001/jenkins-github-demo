d4ae6470bd5069d22d25780449512cf7bd03e577
EC2 Default User <ec2-user@ip-172-31-7-145.ap-south-1.compute.internal>
2025-06-13 07:22:31 +0000

    final test

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 echo 'Hello from Jenkins Build!'
 echo 'This is a test change for Jenkins auto build'
 echo 'This is a test change for Jenkins auto build2'
+echo 'This is a test change for Jenkins auto build3'
